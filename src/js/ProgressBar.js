e6ee3a73f8a26d58057cc2c63998e6d1b74deb27
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-02-03 15:45:30 +0100

    The bars change when the buttons are pressed down, not just clicked

--- src/js/ProgressBar.js
+++ src/js/ProgressBar.js
@@ -4,11 +4,13 @@ export class ProgressBar {
     this.gradient = gradient;
     this.maxValue = maxValue;
     this.color = color;
+    this.interval = null;
     //@TODO no.4 make it possible to create progress bar with any default value and gradient. these values should be passed to constructor
     //@TODO no.5 make it possible to create progress bar with any maximum value. once using maximum value, make sure the width of knob element is calculated correctly
 
     this.element = document.createElement("div");
     this.refreshElement();
+    this.counter = 0;
   }
 
   refreshElement() {
@@ -27,15 +29,28 @@ export class ProgressBar {
       </div>
     `;
 
+    //accessing the plus button
     const plusBtn = this.element.querySelector(".btn-plus");
-    plusBtn.addEventListener("click", () => {
-      this.value = Math.min(this.maxValue, this.value + this.gradient);
-      this.refreshElement();
+    plusBtn.addEventListener("mousedown", () => {
+      this.interval = setInterval(() => {
+        this.value = Math.min(this.maxValue, this.value + this.gradient);
+        this.refreshElement();
+      }, 50);
     });
+    plusBtn.addEventListener("mouseup", () => {
+      clearInterval(this.interval);
+    });
+
+    //accessing the minus button;
     const minusBtn = this.element.querySelector(".btn-minus");
-    minusBtn.addEventListener("click", () => {
-      this.value = Math.max(0, this.value - this.gradient);
-      this.refreshElement();
+    minusBtn.addEventListener("mousedown", () => {
+      this.interval = setInterval(() => {
+        this.value = Math.max(0, this.value - this.gradient);
+        this.refreshElement();
+      }, 50);
+    });
+    minusBtn.addEventListener("mouseup", () => {
+      clearInterval(this.interval);
     });
     //@TODO no.3 implement minusBtn listener. if you assign new value to property value, make sure, it is not greater then 10 (or any other maximum value)
   }
--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,9 @@ import { ProgressBar } from "./ProgressBar";
 
 document.addEventListener("DOMContentLoaded", () => {
   const button = document.querySelector(".color-button");
-  const progressBarRed = new ProgressBar(7, 1, 17, "red");
-  const progressBarGreen = new ProgressBar(7, 1, 17, "green");
-  const progressBarBlue = new ProgressBar(7, 1, 17, "blue");
+  const progressBarRed = new ProgressBar(126, 1, 255, "red");
+  const progressBarGreen = new ProgressBar(126, 1, 255, "green");
+  const progressBarBlue = new ProgressBar(126, 1, 255, "blue");
   document.querySelector("body").appendChild(progressBarRed.element);
   document.body.appendChild(progressBarGreen.element);
   document.body.appendChild(progressBarBlue.element);
@@ -12,14 +12,14 @@ document.addEventListener("DOMContentLoaded", () => {
   //adding the button the functionality
   button.addEventListener("click", () => {
     //accessing the values
-    const red = progressBarRed.value * 15;
-    const green = progressBarGreen.value * 15;
-    const blue = progressBarBlue.value * 15;
+    const red = progressBarRed.value;
+    const green = progressBarGreen.value;
+    const blue = progressBarBlue.value;
 
     //adding the background to the button
     button.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
     //changing the color of the text in the button, so it is at least a bit visible
-    if (red / 15 > 8 || blue / 15 > 8 || green / 15 > 8) {
+    if (red > 126 || blue > 126 || green > 126) {
       button.style.color = "black";
     } else {
       button.style.color = "white";
