2a35df1caa1ae8986805835f6a8082d80e6c7edc
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-02-03 17:11:36 +0100

    Callback function, that changes the button background automatically

--- src/js/ProgressBar.js
+++ src/js/ProgressBar.js
@@ -1,10 +1,12 @@
 export class ProgressBar {
-  constructor(value, gradient, maxValue, color) {
+  constructor(value, gradient, maxValue, color, updateColors) {
     this.value = value;
     this.gradient = gradient;
     this.maxValue = maxValue;
     this.color = color;
     this.interval = null;
+    //accessing the function created from outside
+    this.updateColors = updateColors;
     //@TODO no.4 make it possible to create progress bar with any default value and gradient. these values should be passed to constructor
     //@TODO no.5 make it possible to create progress bar with any maximum value. once using maximum value, make sure the width of knob element is calculated correctly
 
@@ -35,6 +37,8 @@ export class ProgressBar {
       this.interval = setInterval(() => {
         this.value = Math.min(this.maxValue, this.value + this.gradient);
         this.refreshElement();
+        //using the function and providing the attributes
+        this.updateColors(this.color, this.value);
       }, 50);
     });
     plusBtn.addEventListener("mouseup", () => {
@@ -48,6 +52,7 @@ export class ProgressBar {
       this.interval = setInterval(() => {
         this.value = Math.max(0, this.value - this.gradient);
         this.refreshElement();
+        this.updateColors(this.color, this.value);
       }, 50);
     });
     minusBtn.addEventListener("mouseup", () => {
--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,26 @@
 import { ProgressBar } from "./ProgressBar";
 
 document.addEventListener("DOMContentLoaded", () => {
+  //creating an object which tracks the different values of bars
+  const colors = {
+    red: 126,
+    green: 126,
+    blue: 126,
+  };
+
+  //creating function, that updates the values of the colors object and updates the background of the button
+  const updateColors = (color, value) => {
+    colors[color] = value;
+
+    button.style.backgroundColor = `rgb(${colors.red},${colors.green},${colors.blue})`;
+  };
+
   const button = document.querySelector(".color-button");
-  const progressBarRed = new ProgressBar(126, 1, 255, "red");
-  const progressBarGreen = new ProgressBar(126, 1, 255, "green");
-  const progressBarBlue = new ProgressBar(126, 1, 255, "blue");
+  //creation of progress bars, which are given the created updateColors function as an argument
+  const progressBarRed = new ProgressBar(126, 1, 255, "red", updateColors);
+  const progressBarGreen = new ProgressBar(126, 1, 255, "green", updateColors);
+  const progressBarBlue = new ProgressBar(126, 1, 255, "blue", updateColors);
   document.querySelector("body").appendChild(progressBarRed.element);
   document.body.appendChild(progressBarGreen.element);
   document.body.appendChild(progressBarBlue.element);
-
-  //Interval that changes the color based on the current rgb values
-  setInterval(() => {
-    //accessing the values
-    const red = progressBarRed.value;
-    const green = progressBarGreen.value;
-    const blue = progressBarBlue.value;
-
-    //adding the background to the button
-    button.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-    //changing the color of the text in the button, so it is at least a bit visible
-    if (red > 126 || blue > 126 || green > 126) {
-      button.style.color = "black";
-    } else {
-      button.style.color = "white";
-    }
-  }, 50);
-  // @TODO no.1 create new instance of ProgressBar and append its right one property to body element
-  // @TODO no.2 fix missing icon on plus button
 });
